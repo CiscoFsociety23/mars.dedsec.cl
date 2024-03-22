d73d7fc9f0e7d9ed0a3c416ed4d89b3d5fe056a2
 Marco Figueroa <marcofigueroa23@icloud.com>
2024-03-22 03:11:58 -0300

    feature: adding crud for users service

--- src/api/mars/controllers/usersController.ts
+++ src/api/mars/controllers/usersController.ts
@@ -1,12 +1,51 @@
 import express, { Request, Response, Router } from 'express';
+import { UserService } from '../services/usersService';
+import { Users, UserBody, ServiceResponse } from '../interfaces/models/users';
 
 const userController: Router = express.Router();
+const userService: UserService = new UserService();
 
-userController.get('/', (req: Request, res: Response) => {
+userController.get('/', async (req: Request, res: Response) => {
     try {
-        res.json({ status: true });
+        const getUsers: Users[] = await userService.getUsers();
+        res.json(getUsers);
     } catch (error) {
         console.log(`[error]: ${error}`);
+        res.json({ status: false });
+    };
+});
+
+userController.post('/create', async (req: Request, res: Response) => {
+    try {
+        const userBody: UserBody = req.body;
+        const createUser: ServiceResponse = await userService.createUser(userBody);
+        res.json(createUser);
+    } catch (error) {
+        console.log(`[error]: ${error}`);
+        res.json({ status: false });
+    };
+});
+
+userController.put('/update', async (req: Request, res: Response) => {
+    try {
+        const id = req.query.id;
+        const userBody: UserBody = req.body;
+        const updateUser: ServiceResponse = await userService.updateUser(Number(id), userBody);
+        res.json(updateUser)
+    } catch (error) {
+        console.log(`[error]: ${error}`);
+        res.json({ status: false });
+    };
+});
+
+userController.delete('/delete', async (req: Request, res: Response) => {
+    try {
+        const id = req.query.id;
+        const deleteUser: ServiceResponse = await userService.deleteUser(Number(id));
+        res.json(deleteUser);
+    } catch (error) {
+        console.log(`[error]: ${error}`);
+        res.json({ status: false });
     };
 });
 
